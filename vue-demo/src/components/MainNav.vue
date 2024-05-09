1f7088c101bf5fd82aee0f73839a0acd4ff3df17
Michal Vu <sa1g0n@tuta.io>
2024-05-09 17:19:14 +0200

    Hero offset

--- vue-demo/src/components/MainNav.vue
+++ vue-demo/src/components/MainNav.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="w-full text-sm">
+  <header :class="['w-full', 'text-sm', headerHeightClass]">
     <div class="fixed top-0 left-0 w-full h-16 bg-white">
       <div class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8">
         <a :href="url" class="flex h-full items-center text-xl">
@@ -41,6 +41,15 @@ export default {
       menuItems: ["Teams", "Locations", "About us", "How we hire", "Students", "Jobs"],
       isLoggedIn: false
     };
+  },
+  computed: {
+    // SETS OFFSET FOR HERO DEPENDING ON LOGIN STATUS
+    headerHeightClass() {
+      return {
+        "h-16": !this.isLoggedIn,
+        "h-32": this.isLoggedIn
+      };
+    }
   },
   methods: {
     loginUser() {
