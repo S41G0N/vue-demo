753ab68c341f135df7897cf83b75377656506dec
Michal Vu <sa1g0n@tuta.io>
2024-05-19 19:08:08 +0200

    Vue Router Setup

---
+++ vue-demo/src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import HomeView from "@/views/HomeView.vue";
+import ListingsView from "@/views/ListingsView.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: "HomeView"
+  },
+
+  {
+    path: "/sets/listings",
+    name: "Listings",
+    component: "ListingsView"
+  }
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: routes
+});
+
+export default router;
---
+++ vue-demo/src/views/ListingsView.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1 class="text-6xl font-bold">Listings page</h1>
+</template>
+
+<script>
+export default {
+  name: "ListingsView"
+};
+</script>
