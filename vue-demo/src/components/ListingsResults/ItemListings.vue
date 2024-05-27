c282a2657601912a1d2c2acb910ac33205e08618
Michal Vu <sa1g0n@tuta.io>
2024-05-27 19:17:48 +0200

    Axios test

--- vue-demo/src/components/ListingsResults/ItemListings.vue
+++ vue-demo/src/components/ListingsResults/ItemListings.vue
@@ -9,9 +9,18 @@
 </template>
 
 <script>
+import axios from "axios";
 import EachListing from "@/components/ListingsResults/EachListing.vue";
+
 export default {
   name: "ItemListings",
-  components: { EachListing }
+  components: { EachListing },
+  data() {
+    return { sets: [] };
+  },
+  async mounted() {
+    const response = await axios.get("http://localhost:3000/sets");
+    this.sets = response.data;
+  }
 };
 </script>
