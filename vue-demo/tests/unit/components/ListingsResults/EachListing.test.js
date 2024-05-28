73c8bee275eeb076c8bd1547f45a927a1ed55df4
Michal Vu <sa1g0n@tuta.io>
2024-05-28 17:12:04 +0200

    Improved Testing for Individual Listings

--- vue-demo/tests/unit/components/ListingsResults/EachListing.test.js
+++ vue-demo/tests/unit/components/ListingsResults/EachListing.test.js
@@ -1,16 +1,38 @@
 import { render, screen } from "@testing-library/vue";
 import EachListing from "@/components/ListingsResults/EachListing.vue";
+import { RouterLinkStub } from "@vue/test-utils";
 
 describe("EachListing", () => {
-  it("renders listing title", () => {
+  /* Return a prop object */
+  const createMockProps = (customProps = {}) => ({
+    title: "Test title",
+    locations: ["Location1", "Location2"],
+    /*Ovevrite with custom propmpts*/
+    ...customProps
+  });
+  const renderWithStubs = (customProps) => {
     render(EachListing, {
+      global: {
+        stubs: {
+          "router-link": RouterLinkStub
+        }
+      },
       props: {
         listingObject: {
-          title: "Test Title",
-          locations: ["Test", "Test"]
+          ...customProps
         }
       }
     });
-    expect(screen.getByText("Test Title")).toBeInTheDocument;
+  };
+  it("renders listing title", () => {
+    const customProps = createMockProps({ title: "My Title" });
+    renderWithStubs(customProps);
+    expect(screen.getByText("My Title")).toBeInTheDocument;
+  });
+
+  it("renders location", () => {
+    const customProps = createMockProps({ locations: ["My Location 1", "My Location 2"] });
+    renderWithStubs(customProps);
+    expect(screen.getByText("My Location 1")).toBeInTheDocument;
   });
 });
