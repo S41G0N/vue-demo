294b7ac0d1ef7c6db4959ee1c36e673fc1fb9400
Michal Vu <sa1g0n@tuta.io>
2024-05-29 21:09:42 +0200

    Fixing tests for pagination

--- vue-demo/tests/unit/components/ListingsResults/ItemListings.test.js
+++ vue-demo/tests/unit/components/ListingsResults/ItemListings.test.js
@@ -8,27 +8,42 @@ import { RouterLinkStub } from "@vue/test-utils";
 vi.mock("axios");
 
 describe("ItemListings", () => {
-  const renderWithPrompts = () => {
+  const makeRoute = (customParams = {}) => ({
+    query: {
+      page: "1",
+      ...customParams
+    }
+  });
+
+  const renderWithPrompts = (customRoute = {}) => {
     render(ItemListings, {
       global: {
         stubs: {
           "router-link": RouterLinkStub
+        },
+        mocks: {
+          $route: customRoute
         }
       }
     });
   };
+
   it("fetches listings", () => {
     axios.get.mockResolvedValue({ data: [] });
-    renderWithPrompts();
+    renderWithPrompts(makeRoute());
     expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/sets");
   });
 
   /* Async to make sure this code runs after axios.get and DOM to get updated */
   it("create a listing for every set", async () => {
-    axios.get.mockResolvedValue({ data: Array(10).fill({ locations: ["Hello", "World"] }) });
-    renderWithPrompts();
+    const listingsOnPage = 10;
 
+    axios.get.mockResolvedValue({
+      data: Array(listingsOnPage).fill({ locations: ["Hello", "World"] })
+    });
+
+    renderWithPrompts(makeRoute({ page: "1" }));
     const totalListings = await screen.findAllByRole("listitem");
-    expect(totalListings).toHaveLength(10);
+    expect(totalListings).toHaveLength(listingsOnPage);
   });
 });
