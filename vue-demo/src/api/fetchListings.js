0f304025e47aa73dc8a5a96238fdf3d6657aa229
Michal Vu <sa1g0n@tuta.io>
2024-06-01 23:55:02 +0200

    Tests for API call to fetch listings

--- vue-demo/src/api/fetchListings.js
+++ vue-demo/src/api/fetchListings.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const fetchListings = async () => {
   const baseURL = import.meta.env.VITE_APP_API_URL;
   const response = await axios.get(`${baseURL}/sets`);
+  return response.data;
 };
 
 export default fetchListings;
--- vue-demo/tests/api/fetchListings.test.js
+++ vue-demo/tests/api/fetchListings.test.js
@@ -3,10 +3,25 @@ import axios from "axios";
 vi.mock("axios");
 
 import fetchListings from "@/api/fetchListings";
+import { beforeEach } from "vitest";
 
 describe("Fetch listings", () => {
+  beforeEach(() => {
+    axios.get.mockResolvedValue({
+      data: [
+        {
+          id: 1,
+          title: "Test title"
+        }
+      ]
+    });
+  });
   it("fetches listings that meet criteria", async () => {
     await fetchListings();
     expect(axios.get).toHaveBeenCalledWith("http://myfakeapi.com/sets");
+  });
+  it("extracts listings from response", async () => {
+    const data = await fetchListings();
+    expect(data).toEqual([{ id: 1, title: "Test title" }]);
   });
 });
