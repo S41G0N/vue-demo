115fa3f29381b76b7c51823c117744b6e8531e06
Michal Vu <sa1g0n@tuta.io>
2024-06-04 23:57:18 +0200

    Tests for API requests & separating request logic

--- vue-demo/src/stores/listings.js
+++ vue-demo/src/stores/listings.js
@@ -1,7 +1,15 @@
 import { defineStore } from "pinia";
+import fetchListings from "@/api/fetchListings";
 
+export const FETCH_LISTINGS = "FETCH_LISTINGS";
 export const useListingsStore = defineStore("listings", {
   state: () => ({
     listings: []
-  })
+  }),
+  actions: {
+    async [FETCH_LISTINGS]() {
+      const listings = await fetchListings();
+      this.listings = listings;
+    }
+  }
 });
--- vue-demo/tests/unit/stores/listings.test.js
+++ vue-demo/tests/unit/stores/listings.test.js
@@ -1,5 +1,8 @@
 import { createPinia, setActivePinia } from "pinia";
 import { useListingsStore } from "@/stores/listings";
+import axios from "axios";
+
+vi.mock("axios");
 
 describe("state", () => {
   beforeEach(() => {
@@ -9,4 +12,21 @@ describe("state", () => {
     const store = useListingsStore();
     expect(store.listings).toEqual([]);
   });
+});
+
+describe("actions", () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+
+  describe("FETCH_LISTINGS", () => {
+    it("stores listings from an API request using axios", async () => {
+      axios.get.mockResolvedValue({ data: ["Test Listing 1", "Test Listing 2"] });
+      const store = useListingsStore();
+      await store.FETCH_LISTINGS();
+      expect(store.listings).toEqual(["Test Listing 1", "Test Listing 2"]);
+    });
+  });
+
+  it("stores listings", () => {});
 });
