dd1424b2839e17f0e8b9086a54e8a3e6c4fb8969
Michal Vu <sa1g0n@tuta.io>
2024-06-08 02:17:38 +0200

    Accordion styling

--- vue-demo/src/components/Shared/CollapsibleAccordion.vue
+++ vue-demo/src/components/Shared/CollapsibleAccordion.vue
@@ -1,8 +1,31 @@
 <template>
-  <div>My Accordion</div>
+  <div class="border-b border-solid border-brand-gray-2">
+    <div class="flex cursor-pointer flex-wrap items-center justify-between" @click="open">
+      <h3 class="text-base font-semibold">Themes</h3>
+      <font-awesome-icon :icon="caretIcon" />
+    </div>
+    <div v-if="isOpen" class="mt-5 w-full">Child</div>
+  </div>
 </template>
 <script>
 export default {
-  name: "CollapsibleAccordion"
+  name: "CollapsibleAccordion",
+
+  data() {
+    return {
+      isOpen: false
+    };
+  },
+  computed: {
+    caretIcon() {
+      console.log(this.isOpen);
+      return this.isOpen ? "magnifying-glass" : "angle-down";
+    }
+  },
+  methods: {
+    open() {
+      this.isOpen = !this.isOpen;
+    }
+  }
 };
 </script>
