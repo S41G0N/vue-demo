bb088f3f53e5ea30066105f4f4850d8482d0b71f
Michal Vu <sa1g0n@tuta.io>
2024-06-16 18:37:46 +0200

    Test for selecting checkboxes & reformat -> adding helper function

--- vue-demo/tests/unit/components/ListingsResults/ListingsFilterSidebarMinifigures.test.js
+++ vue-demo/tests/unit/components/ListingsResults/ListingsFilterSidebarMinifigures.test.js
@@ -3,25 +3,47 @@ import userEvent from "@testing-library/user-event";
 import { createTestingPinia } from "@pinia/testing";
 import { describe } from "vitest";
 import { useListingsStore } from "@/stores/listings";
+import { useUserStore } from "@/stores/user";
 
 import ListingsFilterSidebarMinifigures from "@/components/ListingsResults/ListingsFilterSidebar/ListingsFilterSidebarMinifigures.vue";
 
-describe("ListingsFilterSidebar", () => {
-  it("renders a unique list of minifigs in the filter", async () => {
+describe("ListingsFilterSidebarMinifigures", () => {
+  const renderListingsFilterSidebarMinifigures = () => {
     const pinia = createTestingPinia();
-    const store = useListingsStore();
-    store.MINIFIG_COUNT = new Set(["1", "2"]);
+    const listingsStore = useListingsStore();
+    const userStore = useUserStore();
+
     render(ListingsFilterSidebarMinifigures, {
       global: {
         plugins: [pinia],
         stubs: { FontAwesomeIcon: true }
       }
     });
+    return { listingsStore, userStore };
+  };
+
+  it("renders a unique list of minifigs in the filter", async () => {
+    const { listingsStore } = renderListingsFilterSidebarMinifigures();
+    listingsStore.MINIFIG_COUNT = new Set(["1", "2"]);
+
     const button = screen.getByRole("button", { name: /minifigures/i });
     await userEvent.click(button);
 
     const minifigFilters = screen.getAllByRole("listitem");
     const minifigCount = minifigFilters.map((node) => node.textContent);
     expect(minifigCount).toEqual(["1", "2"]);
+  });
+
+  it("tests checkboxes", async () => {
+    const { listingsStore, userStore } = renderListingsFilterSidebarMinifigures();
+    listingsStore.MINIFIG_COUNT = new Set(["1", "2"]);
+
+    const button = screen.getByRole("button", { name: /minifigures/i });
+    await userEvent.click(button);
+
+    const oneMinifigureCheckbox = screen.getByRole("checkbox", { name: /1/i });
+    await userEvent.click(oneMinifigureCheckbox);
+
+    expect(userStore.ADD_SELECTED_FILTERS).toHaveBeenCalledWith(["1"]);
   });
 });
