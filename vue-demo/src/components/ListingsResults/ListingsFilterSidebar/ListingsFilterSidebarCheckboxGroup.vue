68e1ab8aa2bb81086bb738531562c9b369f8aa0e
Michal Vu <sa1g0n@tuta.io>
2024-06-25 12:27:17 +0200

    Create a new reusable component ListingsFilterSidebarCheckboxGroup.vue

---
+++ vue-demo/src/components/ListingsResults/ListingsFilterSidebar/ListingsFilterSidebarCheckboxGroup.vue
@@ -0,0 +1,50 @@
+<template>
+  <collapsible-accordion :header="header">
+    <div class="mt-5">
+      <fieldset>
+        <ul class="flex flex-row flex-wrap">
+          <li v-for="filter in availableFilters" :key="filter" class="h-8 w-1/2">
+            <input
+              :id="filter"
+              v-model="selectedFilters"
+              :value="filter"
+              type="checkbox"
+              class="mr-3"
+              @change="selectFilter"
+            />
+            <label :for="filter"> {{ filter }}</label>
+          </li>
+        </ul>
+      </fieldset>
+    </div>
+  </collapsible-accordion>
+</template>
+
+<script setup>
+import CollapsibleAccordion from "@/components/Shared/CollapsibleAccordion.vue";
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+
+const props = defineProps({
+  header: {
+    type: String,
+    required: true
+  },
+  availableFilters: {
+    type: Set,
+    required: true
+  },
+  action: {
+    type: Function,
+    required: true
+  }
+});
+
+const selectedFilters = ref([]);
+const router = useRouter();
+
+const selectFilter = () => {
+  props.action(selectedFilters.value);
+  router.push({ name: "Listings" });
+};
+</script>
